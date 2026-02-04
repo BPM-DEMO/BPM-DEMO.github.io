e3a548b0d4e4fc5b5af7b452df2668984f6b2bf0
BPM-DEMO <sgnext.momiyama@gmail.com>
2026-02-04 13:08:26 +0900

    Add files via upload

---
+++ Scripts/bpm-helper.js
@@ -0,0 +1,27 @@
+﻿// Scripts/bpm-helper.js
+
+async function checkAndNavigate(baseName) {
+    const extensions = ['.html', '.aspx'];
+    let targetUrl = '';
+
+    for (let ext of extensions) {
+        const url = baseName + ext;
+        try {
+            // ファイルが存在するか確認
+            const response = await fetch(url, { method: 'HEAD' });
+            if (response.ok) {
+                targetUrl = url;
+                break;
+            }
+        } catch (error) {
+            console.log(url + " は見つかりません。");
+        }
+    }
+
+    if (targetUrl) {
+        // 見つかったら移動
+        window.location.href = targetUrl;
+    } else {
+        alert("遷移先のファイル（.html または .aspx）が見つかりません。");
+    }
+}
